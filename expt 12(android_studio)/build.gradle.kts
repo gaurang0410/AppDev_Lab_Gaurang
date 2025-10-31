e820e234274faa0d8a875498da4082c3c10979f1
gaurang0410 <gaurangkhanolkar.gk@gmail.com>
2025-10-31 21:41:47 +0530

    Add files via upload

---
+++ expt 12(android_studio)/build.gradle.kts
@@ -0,0 +1,8 @@
+// File: C:/Users/Gaurang Khanolkar/StudioProjects/expt_12/build.gradle.kts
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.gms.services) apply false
+}
