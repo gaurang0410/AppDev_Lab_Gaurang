3bd6bf73b483a48f990f6abb847ad6b170ff7805
gaurang0410 <gaurangkhanolkar.gk@gmail.com>
2025-11-02 17:21:53 +0530

    Delete expt 12(react_native) directory

--- expt 12(react_native)/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Stack } from "expo-router";
-import "@react-native-firebase/app";
-
-export default function RootLayout() {
-  return <Stack />;
-}
--- expt 12(react_native)/google_services.js
+++
@@ -1,8 +0,0 @@
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-
-export const configureGoogleSignIn = () => {
-  GoogleSignin.configure({
-    // Get this from your google-services.json file 
-    webClientId: '514828497551-mtkfjvnccanhvp057h1ej31b49c5dvt6.apps.googleusercontent.com',
-  });
-};
